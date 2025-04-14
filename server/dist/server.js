b9d8be038f296a6f8b3c3a52e1b58d6f8da12799
Mudit Sharma <115790300+mudit203@users.noreply.github.com>
2025-04-14 13:04:38 +0530

    minor

--- server/dist/server.js
+++ server/dist/server.js
@@ -20,6 +20,11 @@ const port = process.env.PORT || 3001;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 // Endpoint to fetch file contents
+app.get('/', (req, res) => {
+   return res.status(200).json({
+    message:"i am from banckend"
+   });
+});
 app.get('/api/file', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const filePath = req.query.path;
@@ -29,15 +34,14 @@ app.get('/api/file', (req, res) => __awaiter(void 0, void 0, void 0, function* (
         // Read the file contents
         const content = yield (0, promises_1.readFile)(filePath, 'utf-8');
         res.json({ content });
+
     }
     catch (error) {
         console.error('Error reading file:', error);
         res.status(500).json({ error: 'Failed to read file' });
     }
 }));
-app.get('/', (req, res) => {
-    res.send('I am from backend');
-});
+
 app.post('/api/file', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { filePath, content } = req.body;
